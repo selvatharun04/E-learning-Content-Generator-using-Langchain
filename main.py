30ac66ecc8cbbfb544b55b3a046b9a05c2da518b
selvatharun2812 <167118375+selvatharun04@users.noreply.github.com>
2025-02-22 20:13:47 +0530

    second commit

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ llm= ChatGoogleGenerativeAI(model='gemini-pro',google_api_key=os.getenv("API_KEY
 
 def generate_lesson(subject,topic,level):
     lesson_prompt=PromptTemplate(input_variables=[subject,topic,level],
-                                 template="Create a lesson on {subject} about {topic} for {level} students that should include Lesson Objectives,Explanation on topic with exaamples")
+                                 template="Create a lesson on {subject} about {topic} for {level} students that should include Lesson Objectives,Explanation on topic with examples")
     lesson_chain= LLMChain(llm=llm,prompt=lesson_prompt)
     return lesson_chain.run({"subject": subject, "topic": topic, "level": level})
 
-def generate_quiz(subject,topic,level):
-    quiz_prompt=PromptTemplate(input_variables=[subject,topic,level],
-                               template="Create a quiz on {subject} on {topic} for {level} students that should include 5 Multiple Choice Questions,5 true or false questiona and 5 fill in the blanks")
+def generate_quiz(subject,topic,level,no_mcq,no_tof,no_fib):
+    quiz_prompt=PromptTemplate(input_variables=[subject,topic,level,no_mcq,no_tof,no_fib],
+                               template="Create a quiz on {subject} on {topic} for {level} students that should include {no_mcq} Multiple Choice Questions,{no_tof} true or false questions and {no_fib} fill in the blanks")
     quiz_chain=LLMChain(llm=llm,prompt=quiz_prompt)
-    return quiz_chain.run({"subject": subject, "topic": topic, "level": level})
+    return quiz_chain.run({"subject": subject, "topic": topic, "level": level,"no_mcq": no_mcq ,"no_tof": no_tof,"no_fib": no_fib})
 
 def generate_summary(subject,topic,level):
     summary_prompt=PromptTemplate(input_variables=[subject,topic,level],
@@ -36,10 +36,10 @@ def extract_text_from_pdf(pdf_file):
         text += page.extract_text() + "\n"
     return text
 
-def generate_quiz_from_pdf(pdf_file):
+def generate_quiz_from_pdf(pdf_file,no_mcq_pdf,no_tof_pdf,no_fib_pdf):
     text=extract_text_from_pdf(pdf_file)
-    quiz_prompt=PromptTemplate(input_variables=[text],
-                               template="Create a quiz on the {text} that should include 5 Multiple Choice Questions,5 true or false questiona and 5 fill in the blanks")
+    quiz_prompt=PromptTemplate(input_variables=[text,no_mcq_pdf,no_tof_pdf,no_fib_pdf],
+                               template="Create a quiz on the {text} that should include {no_mcq_pdf} Multiple Choice Questions,{no_tof_pdf} true or false questiona and {no_fib_pdf} fill in the blanks")
     quiz_chain=LLMChain(llm=llm,prompt=quiz_prompt)
     return quiz_chain.run({"text": text})
 
@@ -57,27 +57,41 @@ subject=st.text_input("Enter Name of Subject:")
 topic=st.text_input("Enter Topic of the Subject:")
 level=st.radio("Select the level of students",["Primary School","High School","Bachelor's Degree","Master's Degree","Doctorate Degree"])
 
+no_mcq=st.number_input("Enter Number of Multiple Choice Questions needed for Quiz",min_value=0,max_value=10)
+no_tof=st.number_input("Enter Number of True or False Questions needed for Quiz",min_value=0,max_value=10)
+no_fib=st.number_input("Enter Number of Fill in the Blanks needed for Quiz",min_value=0,max_value=10)
+
 if st.button("Generate Lesson"):
     st.subheader("Generated Lesson")
     st.write(generate_lesson(subject,topic,level))
 
 if st.button("Generate Quiz"):
     st.subheader("Generated Quiz")
-    st.write(generate_quiz(subject,topic,level))
+    st.write(generate_quiz(subject,topic,level,no_mcq,no_tof,no_fib))
 
 if st.button("Generate Summary"):
     st.subheader("Generated Summary")
     st.write(generate_summary(subject,topic,level))
     
+st.header("Upload PDF File to Generate Quiz and Summary")
 pdf_file=st.file_uploader("Upload PDF File",type=['pdf'])
+st.subheader("Note: The PDF File should contain text to generate Quiz and Summary after that click on the below buttons to generate Quiz and Summary")
+
+no_mcq_pdf=st.number_input("Enter Number of Multiple Choice Questions needed for Quiz",min_value=0,max_value=10)
+no_tof_pdf=st.number_input("Enter Number of True or False Questions needed for Quiz",min_value=0,max_value=10)
+no_fib_pdf=st.number_input("Enter Number of Fill in the Blanks needed for Quiz",min_value=0,max_value=10)
+
 if pdf_file:
     if st.button("Generate Quiz from PDF"):
         st.subheader("Generated Quiz from PDF")
-        st.write(generate_quiz_from_pdf(pdf_file))
+        st.write(generate_quiz_from_pdf(pdf_file,no_mcq_pdf,no_tof_pdf,no_fib_pdf))
         
     if st.button("Generate Summary from PDF"):
         st.subheader("Generated Summary from PDF")
         st.write(generate_summary_from_pdf(pdf_file))
+        
+        
+
         
         
 
