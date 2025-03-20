439d65fe712d30ada234d31e804d1caafde41013
SelvaTharun <selvatharunrajesh@gmail.com>
2025-03-20 11:57:43 +0530

    Set

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="Home Page", page_icon="🏠")
+st.title("E-Learning Content Geneator Using Langchain and Streamlit")
+
+st.subheader("This is a web application that generates educational content using Langchain and Streamlit.")
+st.subheader("You can generate lessons, quizzes, summaries, flashcards, and translations from PDF files.")
+st.subheader("You can also translate text to different languages.")
+st.subheader("To get started, select a page from the sidebar.")
+
+
+
+
